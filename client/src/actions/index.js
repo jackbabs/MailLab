7393cb0bd376a4d7e448e75c45eb68bfaa065f9c
jackbaber <thejackbabs@gmail.com>
2018-04-12 16:40:23 +0100

    posting form values to backend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -13,6 +13,9 @@ export const handleToken = (token) => async dispatch => {
 }
 
 export const submitSurvey = values => {
-  return { type: 'submit_survey' }
+  const res = await axios.post('/api/surveys', values)
+
+  dispatch({ type: FETCH_USER, payload: res.data })
 }
+
 
